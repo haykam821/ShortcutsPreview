f5a7a1f6cb9febb0fae4479a07046493365fd5a2
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-06-19 12:08:02 -0400

    Fix the function in logging-gsd.js always returning undefined

--- logging-gsd.js
+++ logging-gsd.js
@@ -4,10 +4,12 @@ const { getShortcutDetails } = require("shortcuts.js");
 	* Fetches a shortcut while also logging its status.
 	* @param logger The logger to log to.
 **/
-module.exports = (logger, ...arguments) => {
-	return getShortcutDetails(...arguments).then(shortcut => {
+module.exports = (logger, ...args) => {
+	return getShortcutDetails(...args).then(shortcut => {
 		logger("Fetched the '%s' shortcut.", shortcut.name);
-	}).catch(() => {
+		return shortcut;
+	}).catch(error => {
 		logger("Failed to fetch a shortcut with ID '%s'.", arguments[0]);
+		throw error;
 	});
 };
